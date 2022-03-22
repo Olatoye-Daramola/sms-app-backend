a86c8b0b2d88aefac55fdb7489546802adbfecd5
olatoye <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>
2022-03-22 14:58:09 +0100

    SmsAppException added

---
+++ src/main/java/africa/talentup/smsappbackend/web/exception/SmsAppException.java
@@ -0,0 +1,7 @@
+package africa.talentup.smsappbackend.web.exception;
+
+public class SmsAppException extends RuntimeException {
+    public SmsAppException(String message) {
+        super(message);
+    }
+}
