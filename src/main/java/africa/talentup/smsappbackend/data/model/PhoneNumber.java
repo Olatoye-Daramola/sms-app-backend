e6bc3b7bb3beefce80d04ffc72fdd74ae3cf104c
olatoye <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>
2022-03-22 14:55:10 +0100

    PhoneNumber model updated

--- src/main/java/africa/talentup/smsappbackend/data/model/PhoneNumber.java
+++ src/main/java/africa/talentup/smsappbackend/data/model/PhoneNumber.java
@@ -1,6 +1,12 @@
 package africa.talentup.smsappbackend.data.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,12 +16,18 @@ import javax.persistence.Column;
 
 
 @Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhoneNumber {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(nullable = false)
+    private Integer id;
 
-    @Column(unique = true, nullable = false)
+    @Column(length = 40)
     private String number;
 
     @ManyToOne()
