d91ca1a8655426fcd597567e020c0b26f2a48ccc
olatoye <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>
2022-03-23 20:15:34 +0100

    phone number updated

--- src/main/java/africa/talentup/smsappbackend/data/model/PhoneNumber.java
+++ src/main/java/africa/talentup/smsappbackend/data/model/PhoneNumber.java
@@ -2,6 +2,7 @@ package africa.talentup.smsappbackend.data.model;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,21 +14,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Column;
+import java.io.Serializable;
 
 
 @Entity
+@Builder
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class PhoneNumber {
+public class PhoneNumber implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(nullable = false)
     private Integer id;
 
-    @Column(length = 40)
+    @Column(length = 16)
     private String number;
 
     @ManyToOne()
