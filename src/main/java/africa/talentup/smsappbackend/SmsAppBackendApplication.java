a74a4abaa1f6f20071eaf1cc79588a2d2c33fa2a
olatoye <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>
2022-03-25 12:51:42 +0100

    pom

---
+++ src/main/java/africa/talentup/smsappbackend/SmsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package africa.talentup.smsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsAppBackendApplication.class, args);
+    }
+
+}
